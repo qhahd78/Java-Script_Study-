7b43084b002657f464160b1b1ade8787830891b6
YuMinHa <qhahd78>
2020-08-21 05:13:52 +0900

    If else Function practice

--- index.js
+++ index.js
@@ -4,18 +4,42 @@
 // // console.log(b, a);
 
 // //let = 변경가능 const = 변경불가능 //
-// const title = document.querySelector("#title"); //id 로 찾기 
+const title = document.querySelector("#title"); //id 로 찾기 
 // // title.innerHTML = "Hi! From JS"; // html 안의 title 변경
 // // title.style.color = "red";  // html 조종 가능 (스타일 변경)
 // // document.title = 'I own you' ; // html 조종 가능 (사이트 이름 변경)
 // // // console.dir(title);    //html 의 요소들을 볼 수 있다. 
 
 // // //HTML 을 DOM 객체로 바꿀 수 있다. id 를 통해. 
-// function handleClick() {      //클릭할 때 색이 빨간색으로 바뀐다. 
-//     title.style.color="blue"; 
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR ="#7f8c8d";
+
+function handleClick() {      //클릭하면 
+    const currentColor = title.style.color; // 해당 색을 알려준다. 
+    if (currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
+}
+
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick) //click가 일어날 때 함수호출(빨간색으로 바뀌는)
+}
+init();
+
+// function handleOffline() { 
+//     console.log("Bye bye");
 // }
 
-// title.addEventListener("click", handleClick) //click가 일어날 때 함수호출(빨간색으로 바뀌는)
+// function handleOnline() { 
+//     console.log("Welcome back");
+// }
+
+// window.addEventListener("offline", handleOffline)
+// window.addEventListener("online", handleOnline)
+
 
 // true && ture = true ;    //and 
 // false && true = false; 
@@ -33,15 +57,15 @@
 //     console.log('hi')
 // } else {
 //     console.log('ho')
+// // }
+
+// const age = prompt("How lod are you "); 
+
+// if(age >= 18 && age<=21){ 
+//     console.log("drink but you should not")
+// } else if(age > 21){
+//     console.log("you cannot drink")
+// } else {
+//     console.log("too young")
 // }
-
-const age = prompt("How lod are you "); 
-
-if(age >= 18 && age<=21){ 
-    console.log("drink but you should not")
-} else if(age > 21){
-    console.log("you cannot drink")
-} else {
-    console.log("too young")
-}
 
