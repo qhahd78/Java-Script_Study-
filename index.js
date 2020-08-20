7ef8d083dbdccf917ddbc2aab9a1ffd73d5d1f6f
YuMinHa <qhahd78>
2020-08-21 04:33:43 +0900

    If, else, and, or

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
-// let a = 221;
-// let b = a-5;
-// a=4 ;
-// console.log(b, a);
+// // let a = 221;
+// // let b = a-5;
+// // a=4 ;
+// // console.log(b, a);
 
-//let = 변경가능 const = 변경불가능 //
-const title = document.querySelector("#title"); //id 로 찾기 
-// title.innerHTML = "Hi! From JS"; // html 안의 title 변경
-// title.style.color = "red";  // html 조종 가능 (스타일 변경)
-// document.title = 'I own you' ; // html 조종 가능 (사이트 이름 변경)
-// // console.dir(title);    //html 의 요소들을 볼 수 있다. 
+// //let = 변경가능 const = 변경불가능 //
+// const title = document.querySelector("#title"); //id 로 찾기 
+// // title.innerHTML = "Hi! From JS"; // html 안의 title 변경
+// // title.style.color = "red";  // html 조종 가능 (스타일 변경)
+// // document.title = 'I own you' ; // html 조종 가능 (사이트 이름 변경)
+// // // console.dir(title);    //html 의 요소들을 볼 수 있다. 
 
-// //HTML 을 DOM 객체로 바꿀 수 있다. id 를 통해. 
-function handleClick() {      //클릭할 때 색이 빨간색으로 바뀐다. 
-    title.style.color="red"; 
+// // //HTML 을 DOM 객체로 바꿀 수 있다. id 를 통해. 
+// function handleClick() {      //클릭할 때 색이 빨간색으로 바뀐다. 
+//     title.style.color="blue"; 
+// }
+
+// title.addEventListener("click", handleClick) //click가 일어날 때 함수호출(빨간색으로 바뀌는)
+
+// true && ture = true ;    //and 
+// false && true = false; 
+// true && false = false; 
+// false && false = false;
+
+// true || ture = true ;    //or
+// false || true = true; 
+// true || false = true; 
+// false || false = false;
+
+
+
+// if (true || "yumin"==="yumin") {
+//     console.log('hi')
+// } else {
+//     console.log('ho')
+// }
+
+const age = prompt("How lod are you "); 
+
+if(age >= 18 && age<=21){ 
+    console.log("drink but you should not")
+} else if(age > 21){
+    console.log("you cannot drink")
+} else {
+    console.log("too young")
 }
-
-title.addEventListener("click", handleClick) //click가 일어날 때 함수호출(빨간색으로 바뀌는)
 
